db5285fb472497ba17b409fead763c3513a73016
jun-hyeon <cjhn8918989@gmail.com>
2022-11-15 20:26:59 +0900

    코틀린과 자바 상호운용

--- Interop/src/main/kotlin/Hero.kt
+++ Interop/src/main/kotlin/Hero.kt
@@ -1,5 +1,28 @@
-
-fun main(args: Array<String>){
+@file:JvmName("Hero")
+fun main() {
     val adversary = Jhava()
+    println(adversary.utterGreeting())
 
+    val friendshipLevel = adversary.determineFriendsshipLevel()
+    println(friendshipLevel?.lowercase() ?: "무슨 의미인지 난해하군요.")
+
+    val adversaryHitPoints : Int = adversary.hitPoints
+    println(adversaryHitPoints.dec())
+    println(adversaryHitPoints.javaClass)
+
+    adversary.greeting = "hello, Hero"
+    println(adversary.utterGreeting().lowercase())
+
+    adversary.offerFood()
+}
+
+fun makeProclamation() = "Hello, Monster!"
+
+@JvmOverloads
+fun handOverFood(leftHand: String = "딸기", rightHand : String = "고기"){
+    println("맛있는 $leftHand 와 $rightHand 를 넘겨주었습니다.")
+}
+
+class Spellbook{
+    val spells = listOf("Magic Ms. L", "Lay on Hans")
 }
--- Interop/src/main/kotlin/Jhava.java
+++ Interop/src/main/kotlin/Jhava.java
@@ -1,6 +1,39 @@
+import kotlin.jvm.JvmOverloads;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public class Jhava {
 
+    private int hitPoints = 52489112;
+    private String greeting = "BLARGH";
+    @NotNull
     public String utterGreeting(){
-        return "BLAGGGG";
+        return greeting;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    @Nullable
+    public String determineFriendsshipLevel(){
+        return null;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Hero.makeProclamation());
+
+    }
+
+    public void offerFood(){
+        Hero.handOverFood("피자");
     }
 }
